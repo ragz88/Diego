7c152e277a1856cc5fa69274f6e18a1a82c46b2d
Erik Prinz <erik88d3@gmail.com>
2018-11-06 15:23:20 +0200

    Added Cape To Deigo

---
+++ Diego/Assets/Scripts/ActivateCapeColliders.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateCapeColliders : MonoBehaviour {
+
+    public Collider[] cols;
+    bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!activated)
+        {
+            for (int i = 0; i < cols.Length; i++)
+            {
+                cols[i].enabled = true;
+            }
+            activated = true;
+        }
+	}
+}
