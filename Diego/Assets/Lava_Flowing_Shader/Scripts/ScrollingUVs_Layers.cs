ea6765904108828b43a873fe031cb653b751a6fc
Erik Prinz <erik88d3@gmail.com>
2018-11-13 12:44:30 +0200

    Added Door and Loading Bars. Imported some necessary scripts and shaders

--- Diego/Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
+++ Diego/Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -1,20 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScrollingUVs_Layers : MonoBehaviour 
 {
 	//public int materialIndex = 0;
 	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
 	public string textureName = "_MainTex";
+    public bool isImage = true;
+    Material mat;
 	
 	Vector2 uvOffset = Vector2.zero;
-	
-	void LateUpdate() 
+
+    private void Start()
+    {
+       if (isImage)
+       {
+            mat = GetComponent<Image>().material;
+       }
+       else
+       {
+            mat = GetComponent<Renderer>().material;
+       }
+    }
+
+    void LateUpdate() 
 	{
 		uvOffset += ( uvAnimationRate * Time.deltaTime );
-		if( GetComponent<Renderer>().enabled )
-		{
-			GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, uvOffset );
-		}
-	}
+		//if( GetComponent<Renderer>().enabled )
+		//{
+	    //GetComponent<Renderer>().sharedMaterial.SetTextureOffset( textureName, uvOffset );
+        mat.SetTextureOffset(textureName, uvOffset);
+        //}*/
+    }
 }
