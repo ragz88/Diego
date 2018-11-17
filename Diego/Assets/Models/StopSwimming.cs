94f13b1ee0916a5e59f8178016b485d81f6cf4a5
Roddymal <cv9ruleZ!>
2018-11-17 23:04:23 +0200

    Swimming update

---
+++ Diego/Assets/Models/StopSwimming.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class StopSwimming : MonoBehaviour {
+
+    public GameObject Diego;
+    public RuntimeAnimatorController BaseAnimation;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        Diego.GetComponent<Animator>().runtimeAnimatorController = BaseAnimation;
+    }
+}
