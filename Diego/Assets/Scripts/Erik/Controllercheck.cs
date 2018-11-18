08fb690ba05245fe4552c5e2e76be3bd01292a84
Roddymal <cv9ruleZ!>
2018-11-18 11:28:53 +0200

    Controller script and platforms

---
+++ Diego/Assets/Scripts/Erik/Controllercheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controllercheck : MonoBehaviour
+{
+
+    private int PS4_Controller = 0;
+    public bool controllerConected = false;
+    void Update()
+    {
+        string[] names = Input.GetJoystickNames();
+        for (int x = 0; x < names.Length; x++)
+        {
+            print(names[x].Length);
+            if (names[x].Length == 19)
+            {
+                print("PS4 CONTROLLER IS CONNECTED");
+                PS4_Controller = 1;
+            }
+        }
+
+
+        if (PS4_Controller == 1)
+        {
+            controllerConected = true;
+            //Erik does controller things here
+        }
+        else
+        {
+            
+            controllerConected = false;
+            //There is no controllers
+        }
+    }
+}
