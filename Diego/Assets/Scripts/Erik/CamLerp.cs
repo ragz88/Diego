3ee584c32f67ef4003c0c29e40bf76a9044beaac
Erik Prinz <erik88d3@gmail.com>
2018-11-19 13:52:20 +0200

    Added floating orb

---
+++ Diego/Assets/Scripts/Erik/CamLerp.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamLerp : MonoBehaviour {
+
+    public Transform[] lerpPoints;
+    public float[] delays;
+    public float lerpSpeed;
+    public float slerpSpeed;
+    float timer = 0;
+    int currentPos = 0;
+    bool lerping = false;
+    public bool loop = false;
+    public bool useDelays = true;
+    bool active = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (useDelays)
+        {
+            if (active)
+            {
+                if (lerping)
+                {
+                    transform.position = Vector3.Lerp(transform.position, lerpPoints[currentPos].position, lerpSpeed * Time.deltaTime);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, lerpPoints[currentPos].rotation, slerpSpeed * Time.deltaTime);
+                    if ((Vector3.Distance(transform.position, lerpPoints[currentPos].position) < 0.05f) &&
+                    (Quaternion.Angle(transform.rotation, lerpPoints[currentPos].rotation) < 0.5f))
+                    {
+                        lerping = false;
+                    }
+                }
+                else
+                {
+                    if (timer >= delays[currentPos])
+                    {
+                        switchPos();
+                    }
+                    else
+                    {
+                        timer += Time.deltaTime;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (lerping)
+            {
+                transform.position = Vector3.Lerp(transform.position, lerpPoints[currentPos].position, lerpSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lerpPoints[currentPos].rotation, slerpSpeed * Time.deltaTime);
+                if ((Vector3.Distance(transform.position, lerpPoints[currentPos].position) < 0.05f) &&
+                (Quaternion.Angle(transform.rotation, lerpPoints[currentPos].rotation) < 0.5f))
+                {
+                    lerping = false;
+                }
+            }
+        }
+    }
+
+    void switchPos()
+    {
+        timer = 0;
+        lerping = true;
+        if (currentPos < lerpPoints.Length)
+        {
+            currentPos += 1;
+        }
+        else
+        {
+            if (loop)
+            {
+                currentPos = 0;
+            }
+            else
+            {
+                active = false;
+            }
+        }
+    }
+
+    public void switchPos(int changeAmount)
+    {
+        lerping = true;
+        currentPos = (currentPos + changeAmount) % lerpPoints.Length;
+    }
+}
---
+++ Diego/Assets/Scripts/Erik/OrbHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbHover : MonoBehaviour {
+
+    //public Transform bottom;
+    public float hoverSpeed;
+    public float hoverAmplitude;
+    public float lerpSpeed;
+    public Vector3 recentre; 
+
+    GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+        cube = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Vector3.down);
+        transform.position = Vector3.Lerp(transform.position, cube.transform.position + recentre + 
+            (hoverAmplitude * Vector3.up * Mathf.Sin(Time.time*hoverSpeed)), lerpSpeed * Time.deltaTime);
+	}
+}
