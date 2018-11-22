e9d2da5f7b542946656cc183f9315328fc76b85d
Roddymal <cv9ruleZ!>
2018-11-22 10:27:10 +0200

    Leve1 Updates

--- Diego/Assets/Scripts/Erik/ButtonColourControl.cs
+++ Diego/Assets/Scripts/Erik/ButtonColourControl.cs
@@ -7,18 +7,26 @@ using UnityEngine.UI;
 public class ButtonColourControl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
 
     Color initColour, finColour;
+    MusicOperator musicPlayer;
+    Text text;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         initColour = gameObject.GetComponentInChildren<Text>().color;
+        initColour = new Color(initColour.r, initColour.g, initColour.b, initColour.a - .2f);
         finColour = new Color(initColour.r, initColour.g, initColour.b, 1);
+        finColour = Color.white;
+        text = gameObject.GetComponentInChildren<Text>();
+        musicPlayer = GameObject.FindObjectOfType<MusicOperator>();
+        text.font = musicPlayer.menuFont;
 
     }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+   
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
