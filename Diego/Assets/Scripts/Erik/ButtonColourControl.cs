08599c65b15dfad8570cd3d4f9d76c510d79214e
Erik Prinz <erik88d3@gmail.com>
2018-11-23 23:52:07 +0200

    Fixed menu axis selection. Improved level 4 lag.

--- Diego/Assets/Scripts/Erik/ButtonColourControl.cs
+++ Diego/Assets/Scripts/Erik/ButtonColourControl.cs
@@ -9,13 +9,15 @@ public class ButtonColourControl : MonoBehaviour, IPointerEnterHandler, IPointer
     Color initColour, finColour;
     MusicOperator musicPlayer;
     Text text;
+    //public EventSystem eventSystem;
 
     // Use this for initialization
     void Start () {
         initColour = gameObject.GetComponentInChildren<Text>().color;
         initColour = new Color(initColour.r, initColour.g, initColour.b, initColour.a - .2f);
-        finColour = new Color(initColour.r, initColour.g, initColour.b, 1);
+        //finColour = new Color(initColour.r, initColour.g, initColour.b, 1);
         finColour = Color.white;
+        finColour = new Color(finColour.r, finColour.g, finColour.b, 1);
         text = gameObject.GetComponentInChildren<Text>();
         musicPlayer = GameObject.FindObjectOfType<MusicOperator>();
         text.font = musicPlayer.menuFont;
@@ -25,7 +27,14 @@ public class ButtonColourControl : MonoBehaviour, IPointerEnterHandler, IPointer
 	// Update is called once per frame
 	void Update ()
     {
-   
+        if (EventSystem.current.currentSelectedGameObject == gameObject)
+        {
+            gameObject.GetComponentInChildren<Text>().color = finColour;
+        }
+        else
+        {
+            gameObject.GetComponentInChildren<Text>().color = initColour;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
